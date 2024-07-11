e09871af7434029ed51ede352a08f8ec8303e242
gopika346 <gopikabk1312@gmail.com>
2024-07-11 13:10:50 +0530

    test 3

---
+++ client/src/Component/login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1 className='flex justify-center mt-10 items-center text-xl md:text-xl '>Hello</h1>
+      <button className='bg-red-300 p-2 '></button>
+    </div>
+  )
+}
+
+export default Login
